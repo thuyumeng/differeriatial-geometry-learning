32a4ccec07a31456b5e144801112d60d2280954f
于蒙 <yumeng@yumengdeiMac.local>
2019-06-03 23:17:33 +0800

    导数的Mathematica函数

---
+++ notebook1-2.m
@@ -0,0 +1,18 @@
+(* ::Package:: *)
+
+(* ::Input:: *)
+(*\[Alpha][t_] := {t, t^2, t^3}*)
+(*L[v_] := Sqrt[Simplify[v.v]]*)
+(*D[\[Alpha][t], t]*)
+(*\[Alpha]'[u]*)
+(*L[\[Alpha]'[t]]*)
+(**)
+(*Clear[\[Alpha], f]*)
+(*\[Beta][t_] := \[Alpha][f[t]]*)
+(*Table[D[\[Beta][t], {t,k}], {k, 4}]*)
+(*ColumnForm[%]*)
+(*Do[Print[D[\[Beta][t], {t,k}]], {k,5,6}]*)
+(**)
+
+
+
